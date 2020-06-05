ee20d0c44b3acf1dc495b03d3582e9eb92f6c5ab
Maahir Sachdev <51933563+maahir30@users.noreply.github.com>
2020-06-04 18:59:59 -0700

    Update solver.py

--- MaahirSudokuSolver/solver.py
+++ MaahirSudokuSolver/solver.py
@@ -21,7 +21,8 @@ APPROACH:
 '''
 
 #zeroes indicate a blank space
-board = [
+
+b1 = [
     [5,3,0,0,7,0,0,0,0],
     [6,0,0,1,9,5,0,0,0],
     [0,9,8,0,0,0,0,6,0],
@@ -32,6 +33,33 @@ board = [
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,8,0,0,7,9]
 ]
+
+b2 = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+
+#Choose a board
+
+while True:
+	sudoku_puzzle = input("Which board would you like to have solved, board 1 or 2? Enter an integer for the board of your choice.")
+	if int(sudoku_puzzle) == 1:
+		board = b1
+		break
+	elif int(sudoku_puzzle) == 2:
+		board = b2
+		break
+	else:
+		print("Enter an integer only! Please try again.")
+		continue
 
 
 #prints the sudoku board
